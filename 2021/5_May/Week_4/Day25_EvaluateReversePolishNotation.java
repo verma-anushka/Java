50b8d0919de3b00b37c3529c192ee94f68845f64
Anushka Verma <v.anushka786@gmail.com>
2021-05-28 01:27:26 +0530

    add may challenge: week #4

---
+++ 2021/5_May/Week_4/Day25_EvaluateReversePolishNotation.java
@@ -0,0 +1,76 @@
+
+//Day 25: 
+//Problem Statement: Evaluate Reverse Polish Notation
+
+// Evaluate the value of an arithmetic expression in Reverse Polish Notation.
+
+// Valid operators are +, -, *, and /. Each operand may be an integer or another expression.
+
+// Note that division between two integers should truncate toward zero.
+
+// It is guaranteed that the given RPN expression is always valid. That means the expression would always evaluate to a result, and there will not be any division by zero operation.
+
+ 
+
+// Example 1:
+
+// Input: tokens = ["2","1","+","3","*"]
+// Output: 9
+// Explanation: ((2 + 1) * 3) = 9
+// Example 2:
+
+// Input: tokens = ["4","13","5","/","+"]
+// Output: 6
+// Explanation: (4 + (13 / 5)) = 6
+// Example 3:
+
+// Input: tokens = ["10","6","9","3","+","-11","*","/","*","17","+","5","+"]
+// Output: 22
+// Explanation: ((10 * (6 / ((9 + 3) * -11))) + 17) + 5
+// = ((10 * (6 / (12 * -11))) + 17) + 5
+// = ((10 * (6 / -132)) + 17) + 5
+// = ((10 * 0) + 17) + 5
+// = (0 + 17) + 5
+// = 17 + 5
+// = 22
+ 
+
+// Constraints:
+
+// 1 <= tokens.length <= 104
+// tokens[i] is either an operator: "+", "-", "*", or "/", or an integer in the range [-200, 200].
+
+//Solution:
+class Solution {
+    
+    private Set<String> ops=new HashSet<>(Arrays.asList("+", "-", "*", "/"));
+    
+    public int evalRPN(String[] tokens) {
+     
+        Stack<Integer> st=new Stack<>();
+        
+        for(String t: tokens) {
+        
+            if(!ops.contains(t)) 
+                st.push(Integer.parseInt(t));
+            else {
+            
+                int b=st.pop(), a=st.pop();
+                if(t.equals("+")) 
+                    st.push(a+b);
+                else if(t.equals("-")) 
+                    st.push(a-b);
+                else if(t.equals("*")) 
+                    st.push(a*b);
+                else 
+                    st.push(a/b);
+            
+            }
+        }
+        
+        return st.pop();
+    }
+}
+//Complexity: O(n)
+
+
---
+++ 2021/5_May/Week_4/Day28_MaximumErasureValue.java
@@ -0,0 +1,56 @@
+
+//Day 28: 
+//Problem Statement: Maximum Erasure Value
+
+// You are given an array of positive integers nums and want to erase a subarray containing unique elements. The score you get by erasing the subarray is equal to the sum of its elements.
+
+// Return the maximum score you can get by erasing exactly one subarray.
+
+// An array b is called to be a subarray of a if it forms a contiguous subsequence of a, that is, if it is equal to a[l],a[l+1],...,a[r] for some (l,r).
+
+ 
+
+// Example 1:
+
+// Input: nums = [4,2,4,5,6]
+// Output: 17
+// Explanation: The optimal subarray here is [2,4,5,6].
+// Example 2:
+
+// Input: nums = [5,2,1,2,5,2,1,2,5]
+// Output: 8
+// Explanation: The optimal subarray here is [5,2,1] or [1,2,5].
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 105
+// 1 <= nums[i] <= 104
+
+//Solution:
+class Solution {
+    public int maximumUniqueSubarray(int[] nums) {
+     
+        int l=0, sum=0, ans=0;
+        Map<Integer, Integer> visited=new HashMap<>();
+
+        for(int r=0; r<nums.length; r++) {
+            int val=nums[r];
+            if(visited.containsKey(val)) {
+                int idx=visited.get(val);
+                while(l<=idx) {
+                    visited.remove(nums[l]);
+                    sum-=nums[l++];
+                }
+            }
+            
+            visited.put(val, r);
+            sum+=val;
+            ans=Math.max(ans, sum);
+        }
+        
+        return ans;
+    }
+}
+//Complexity: O(n)
+
